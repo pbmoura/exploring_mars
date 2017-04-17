a4a2ae4de93ec8a3a660dfb7cde11bdd3fde2664
Paulo Moura <pbmoura@gmail.com>
2017-04-17 11:06:20 -0300

    probe turns left

--- lib/probe.ex
+++ lib/probe.ex
@@ -1,3 +1,10 @@
 defmodule Probe.State do
   defstruct x: 0, y: 0, dir: 0
 end
+
+defmodule Probe do
+  def turn_left(probe) do
+    %{probe | dir: if probe.dir == 0 do 3 else probe.dir-1 end}
+  end
+
+end
---
+++ test/probe_test.exs
@@ -0,0 +1,29 @@
+defmodule ProbeTest do
+  use ExUnit.Case
+  doctest Probe
+
+  test "N to W" do
+    probe = %Probe.State{}
+    probe = Probe.turn_left(probe)
+    assert probe == %Probe.State{dir: 3}
+  end
+
+  test "E to N" do
+    probe = %Probe.State{dir: 1}
+    probe = Probe.turn_left(probe)
+    assert probe == %Probe.State{}
+  end
+
+  test "S to E" do
+    probe = %Probe.State{dir: 2}
+    probe = Probe.turn_left(probe)
+    assert probe == %Probe.State{dir: 1}
+  end
+
+  test "W to S" do
+    probe = %Probe.State{dir: 3}
+    probe = Probe.turn_left(probe)
+    assert probe == %Probe.State{dir: 2}
+  end
+
+end
