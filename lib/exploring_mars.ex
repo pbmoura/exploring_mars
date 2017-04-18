1f36db912e8dbb8b8823f9b7b2b44f6ecc9a8a2e
Paulo Moura <pbmoura@gmail.com>
2017-04-17 21:04:49 -0300

    input from stdin. single probe

--- lib/exploring_mars.ex
+++ lib/exploring_mars.ex
@@ -1,18 +1,28 @@
 defmodule ExploringMars do
-  @moduledoc """
-  Documentation for ExploringMars.
-  """
 
-  @doc """
-  Hello world.
+  def request_terrain do
+    [x,y] = for s <- String.split(IO.gets "terrain top-right coordinate (x y)?") do
+      String.to_integer(s)
+    end
+    %Terrain{x_max: x, y_max: y}
+  end
 
-  ## Examples
+  def request_probe do
+    [sx, sy, sd] = String.split(IO.gets "probe position (x y direction)?")
+    x = String.to_integer(sx)
+    y = String.to_integer(sy)
+    d = Probe.Directions.index(sd)
+    %Probe.State{x: x, y: y, dir: d}
+  end
+  def request_route do
+    String.graphemes(String.trim(IO.gets "route (sequence of L,M,R)?"))
+  end
 
-      iex> ExploringMars.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def start do
+    terrain = request_terrain()
+    probe = request_probe()
+    route = request_route()
+    probe = Probe.travel(probe, route, terrain)
+    IO.puts Probe.to_string(probe)
   end
 end
