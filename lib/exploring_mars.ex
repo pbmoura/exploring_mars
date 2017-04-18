ea525cdcfaa13df3751cd115ce7a12c3916e06d2
Paulo Moura <pbmoura@gmail.com>
2017-04-17 21:34:34 -0300

    accepting many probes in sequence

--- lib/exploring_mars.ex
+++ lib/exploring_mars.ex
@@ -8,21 +8,29 @@ defmodule ExploringMars do
   end
 
   def request_probe do
-    [sx, sy, sd] = String.split(IO.gets "probe position (x y direction)?")
-    x = String.to_integer(sx)
-    y = String.to_integer(sy)
-    d = Probe.Directions.index(sd)
-    %Probe.State{x: x, y: y, dir: d}
+    input = String.split(IO.gets "probe position (x y direction)?")
+    case input do
+      [sx, sy, sd] -> {true, %Probe.State{x: String.to_integer(sx), y: String.to_integer(sy), dir: Probe.Directions.index(sd)}}
+      _ -> {false, nil}
+    end
   end
+
   def request_route do
     String.graphemes(String.trim(IO.gets "route (sequence of L,M,R)?"))
   end
 
+  def probe_loop(terrain) do
+    {continue, probe} = request_probe()
+    if continue do
+      route = request_route()
+      probe = Probe.travel(probe, route, terrain)
+      IO.puts Probe.to_string(probe)
+      probe_loop(terrain)
+    end
+  end
+
   def start do
     terrain = request_terrain()
-    probe = request_probe()
-    route = request_route()
-    probe = Probe.travel(probe, route, terrain)
-    IO.puts Probe.to_string(probe)
+    probe_loop(terrain)
   end
 end
